1678694d531c6cd741835a6ad75cbc4f44bcdb25
Ryan Georgi <rgeorgi@gmail.com>
2016-05-04 21:30:16 -0700

    Figuring out how to make Freki globally align stuff...

--- freki_align.py
+++ freki_align.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
+import copy
 import os, re, sys
 from argparse import ArgumentParser
-from collections import defaultdict
+from collections import defaultdict, Iterator
 import importlib
 
 # -------------------------------------------
 # Import the config file
 # -------------------------------------------
+from functools import partial
 
+import pickle
+
+import itertools
 
 from config import *
 
@@ -25,6 +30,35 @@ from xigt import xigtpath
 # -------------------------------------------
 
 
+class FrekiFile(Iterator):
+    def __init__(self, path):
+        self.linedict = {}
+        self.blocks = {}
+        self.fh = open(path, 'r', encoding='utf-8')
+
+        self._load()
+
+    def _load(self):
+        for line in self:
+            pass
+        self.fh.seek(0)
+
+    def __len__(self):
+        return len(list(self.linedict.keys()))
+
+    def __next__(self):
+        data = self.fh.__next__()
+        block = None
+        while data:
+            if data.startswith('doc_id'):
+                block = data.split()[1].split('=')[1]
+            elif data.strip():
+                line, text = re.search('^line=([0-9]+):(.*$)', data).groups()
+                self.linedict[int(line)] = TextLine(text, int(line))
+                self.blocks[int(line)] = block
+                return TextLine(text, int(line))
+
+            data = self.fh.__next__()
 
 # -------------------------------------------
 # Levenshtein Distance
@@ -181,26 +215,64 @@ def find_most_similar(d):
 
     return orig_linenos, best_match
 
+class Configurator(Iterator):
+    def __init__(self, odin_spans, freki_len):
+
+        self.maximums = []
+        self.minimums = []
+        # Iterate backwards through the spans...
+        for span in sorted(odin_spans, reverse=True):
+            if not self.maximums:
+                next_max = freki_len - len(span)
+                next_min = 0
+            else:
+                next_max = self.maximums[0] - len(span)
+                next_min = self.minimums[-1] + len(span)
+
+            self.maximums.insert(0, next_max)
+            self.minimums.append(next_min)
+
+        self.offsets = copy.copy(self.minimums)
+        self.offsets[-1] -= 1
+
+
+
+
+    def __next__(self):
+
+        # Try to increment each offset, carrying over
+        # if it hits the max.
+        i = len(self.offsets)-1
+
+        while i >= 0:
+            if self.offsets[i] < self.maximums[i]:
+                self.offsets[i] += 1
+                break
+            elif self.offsets[i] >= self.maximums[i]:
+                # Increment this 'minimum', if we still can.
+                if self.minimums[i] < self.maximums[i]:
+                    self.minimums[i] += 1
+                # reset this offset to that min.
+                self.offsets[i] = self.minimums[i]
+                # Now, set the i to the next position to the left
+                # and see if we need to repeat
+                i -= 1
+            else:
+                break
+
+        if i < 0:
+            raise StopIteration
+        else:
+            return self.offsets
+
+
+
+
+
 # -------------------------------------------
 # Now, iterate over the pairs.
 # -------------------------------------------
-def renum_checks(check_instances, freki_blocks, match_dict = None, match_f=sys.stdout):
-
-    if match_dict is None:
-        match_dict = {'matches':0, 'compares':0}
-
-    last_freki_index = 0
-
-    # -------------------------------------------
-    # Convert Freki Blocks
-    # -------------------------------------------
-    fbs = []
-    fbmap = {}
-    for freki_block in freki_blocks:
-        fb = FrekiBlock(freki_block)
-        fbs.append(fb)
-        for line in fb.lines:
-            fbmap[line] = fb
+def renum_checks(check_instances, ff : FrekiFile, match_dict = None, match_f=sys.stdout):
 
     # -------------------------------------------
     # Convert to ODIN Blocks
@@ -232,39 +304,100 @@ def renum_checks(check_instances, freki_blocks, match_dict = None, match_f=sys.s
     # Now, step through the cleaned lines and try
     # to match them up with the new data
     # -------------------------------------------
-    for igt in xc:
-        linemap = defaultdict(lambda: {})
-        clean_tier = igt[1]
+
+    # We will keep a dictionary that indexes by [igt#][freki-line][odin-line(s)] = lev. dist
+    # then, we will seek to find the optimal mapping.
+    if False:
+        igtlinemap = defaultdict(partial(defaultdict, partial(defaultdict, partial(int, 'inf'))))
 
 
-        for clean_item in clean_tier:
-            lines = clean_item.attributes['line']
-            clean_text = clean_item.value()
-            for fb in fbs:
-                for lineno in fb.lines:
-                    line_text = fb.lines[lineno]
+        for igt_index, igt in enumerate(xc):
+            clean_tier = igt[1]
+
+            for clean_item in clean_tier:
+                odin_lines = tuple(int(l) for l in clean_item.attributes['line'].split())
+                clean_text = clean_item.value()
+                for freki_lineno, freki_line in sorted(ff.linedict.items(), key=lambda x: x[0]):
 
                     # Remove the whitespace and compare the lines
-                    line_text  = re.sub('\s','', line_text)
+                    freki_text  = re.sub('\s','', freki_line)
                     clean_text = re.sub('\s', '', clean_text)
+                    igtlinemap[igt_index][freki_line.lineno][odin_lines] = levenshtein(freki_text, clean_text)
 
-                    linemap[lines][lineno] = levenshtein(line_text, clean_text)
+        with open('test.pickle', 'wb') as f:
+            pickle.dump(igtlinemap, f)
+        sys.exit()
+
+    else:
+        with open('test.pickle', 'rb') as f:
+            igtlinemap = pickle.load(f)
+
+    # Once we've computed the lev. distance for each pair of lines, now let's
+    # do a search for the best fit, with the constraint that the blocks must
+    # occur in the same order as the odin lines do.
 
 
-        # Find the mapping between line numbers from the original
-        # annotation to the new, freki numbers.
-        orig_lines, freki_lines = find_most_similar(linemap)
+    # First, let's calculate [odin_span][freki_spans]
+    spanmaps = defaultdict(dict)
+
+    for igt_index in igtlinemap.keys():
+
+        for freki_lineno in sorted(igtlinemap[igt_index].keys()):
+            chunk_total = 0
+
+            odin_line_tups = tuple(sorted(igtlinemap[igt_index][freki_lineno]))
+            freki_tup = (freki_lineno, freki_lineno + len(odin_line_tups))
+
+            for i, odin_line_tup in enumerate(odin_line_tups):
+                chunk_total += igtlinemap[igt_index].get(freki_lineno+i, defaultdict(lambda x: float('inf'))).get(odin_line_tup, float('inf'))
 
 
-        # match_f.write('igt_id={}\n'.format(igt.id))
-        for i, orig_line in enumerate(orig_lines):
-            orig_indices = ','.join(orig_line.split())
-            frek_index   = freki_lines[i]
+            spanmaps[odin_line_tups][freki_tup] = chunk_total
 
-            orig_item = xigtpath.find(clean_tier, '//item[@line="{}"]'.format(orig_line))
 
-            match_f.write('{1}:{0}\n'.format(orig_item.attributes['tag'], frek_index))
-        # match_f.write('\n')
+    # Now, let's search for the optimal non-overlapping configuration.
+
+    # c = Configurator(sorted(spanmaps.keys()), len(ff))
+
+
+
+    c = Configurator([(0,1,2),(5,6,7),(9,10,11)], 13)
+    for config in c:
+        print(config)
+
+
+    sys.exit()
+
+
+
+
+        #
+        # # Find the mapping between line numbers from the original
+        # # annotation to the new, freki numbers.
+        # orig_lines, freki_lines = find_most_similar(linemap)
+        #
+        #
+        # # match_f.write('igt_id={}\n'.format(igt.id))
+        # for i, orig_line in enumerate(orig_lines):
+        #     orig_indices = ','.join(orig_line.split())
+        #     frek_index   = freki_lines[i]
+        #
+        #     orig_item = xigtpath.find(clean_tier, '//item[@line="{}"]'.format(orig_line))
+        #
+        #     match_f.write('{1}:{0}\n'.format(orig_item.attributes['tag'], frek_index))
+        # # match_f.write('\n')
+
+
+class TextLine(str):
+    def __new__(cls, seq=None, lineno=None):
+        if seq is None:
+            seq = ''
+        tl = str.__new__(cls, seq)
+        tl.lineno = lineno
+        return tl
+
+
+
 
 
 if __name__ == '__main__':
@@ -275,16 +408,16 @@ if __name__ == '__main__':
 
     filenum = int(os.path.splitext(os.path.basename(args.checkfile))[0])
 
+    freki_path = os.path.join(FREKI_DIR, '{}.txt'.format(filenum))
+    ff = FrekiFile(freki_path)
 
 
     check_data = open(checkfiles[filenum], 'r', encoding='latin-1').readlines()
-    freki_data = open(frekifiles[filenum], 'r', encoding='utf-8').readlines()
 
     with open(os.path.join(MATCH_DIR, str(filenum)+'.matches'), 'w', encoding='utf-8') as f:
 
         check_instances = gather_check_instances(check_data)
-        freki_blocks    = gather_check_instances(freki_data)
 
         match_dict = {'matches':0, 'compares':0}
-        renum_checks(check_instances, freki_blocks, match_dict=match_dict, match_f=f)
+        renum_checks(check_instances, ff, match_dict=match_dict, match_f=f)
 

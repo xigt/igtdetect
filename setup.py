eee751f62f626b19569d1242e4e8d8f5fd02dabf
Ryan Georgi <rgeorgi@gmail.com>
2016-11-29 13:58:07 -0600

    Fixing setup.py by switching to setuptools from distutils, changing version number to MAJOR.MINOR.PATCH, and moving freki from install_requires to dependency_links.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='igt-detect',
-      version='0.1',
+      version='1.0.0',
       description='Line-level classifier for IGT instances, part of RiPLEs pipeline.',
       author='Ryan Georgi',
       author_email='rgeorgi@uw.edu',
       url='https://github.com/xigt/igtdetect',
       install_requires = [
-          'freki',
           'scikit-learn >= 0.18',
           'numpy'
       ],
+      dependency_links = [
+          'https://github.com/xigt/freki'
+      ]
       )
